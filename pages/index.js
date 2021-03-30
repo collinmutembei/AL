98296150033b57a8e1cbfa673f142bd07cd96c10
Collin Mutembei <mutembei.collin@gmail.com>
2021-03-30 22:46:58 +0300

    Prefix server url

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,9 @@
 import Head from "next/head";
-import { getTokenOrRefresh } from "@/lib/token";
-import { ResultReason } from "microsoft-cognitiveservices-speech-sdk";
 import styles from "@/styles/Home.module.css";
+import { sttFromMic } from "@/lib/speech";
 
-const speechsdk = require("microsoft-cognitiveservices-speech-sdk");
-
-export default async function Home() {
-  const tokenObj = await getTokenOrRefresh();
-  const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(
-    tokenObj.authToken,
-    tokenObj.region
-  );
-  speechConfig.speechRecognitionLanguage = "en-US";
-
-  const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-  const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
-
-  let sttFromMic = recognizer.recognizeOnceAsync((result) => {
-    if (result.reason === ResultReason.RecognizedSpeech) {
-      console.log(`RECOGNIZED: Text=${result.text}`);
-    } else {
-      console.log(
-        "ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly."
-      );
-    }
-  });
-
+export default function Home() {
+  sttFromMic();
   return (
     <div className={styles.container}>
       <Head>
@@ -39,7 +17,7 @@ export default async function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="#" className={styles.card} onClick={() => sttFromMic()}>
+          <a href="#" className={styles.card}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"

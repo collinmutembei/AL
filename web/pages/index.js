949af665cc2c1e2899f04a4e47f203b5867d3146
Collin Mutembei <mutembei.collin@gmail.com>
2021-03-31 18:28:49 +0300

    Tweak styling

--- web/pages/index.js
+++ web/pages/index.js
@@ -21,8 +21,6 @@ export default function Home() {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              height="20px"
-              width="20px"
             >
               <path
                 strokeLinecap="round"
@@ -40,14 +38,12 @@ export default function Home() {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              height="20px"
-              width="20px"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+                d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
               />
             </svg>
             <h3>Chat</h3>
